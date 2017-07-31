aaafedc8e7242647b4286d8f3ef5b4c2885d537e
rehamFatima <reham@ebi.ac.uk>
2017-07-31 17:55:03 +0100

    Update extract_chromosomeForChIP.py

--- ChIPSeq_Scripts/extract_chromosomeForChIP.py
+++ ChIPSeq_Scripts/extract_chromosomeForChIP.py
@@ -4,9 +4,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("infile", help="-infile -> The input fasta file")
-parser.add_argument("outfile", help="-outfile -> The output file for chromosome") 
-args= parser.parse_args()
+parser.add_argument("infile", help = "-infile -> The input fasta file")
+parser.add_argument("outfile", help = "-outfile -> The output file for chromosome") 
+args = parser.parse_args()
 
 inputFile = open(args.infile, 'r')
 outputFile = open(args.outfile, 'w')
@@ -16,10 +16,10 @@ for line in inputFile:
         line2 = line.split(",")
         line2 = line2[0].split(" ")
         
-        if(line2[4]=="22"):
+        if(line2[4] == "22"):
             print line
             while True:
-                line=inputFile.next()
+                line = inputFile.next()
 
                 if (line.startswith(">")):
                     break
