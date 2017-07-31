cf5160823209fffb3e6b63dfc669a0eb34b44b0a
rehamFatima <reham@ebi.ac.uk>
2017-07-31 10:41:45 +0100

    Scripts for ChIP seq test data

---
+++ ChIPSeq_Scripts/extract_chromosomeForChIP.py
@@ -0,0 +1,32 @@
+#!usr/bin/python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("infile", help="-infile -> The input fasta file")
+parser.add_argument("outfile", help="-outfile -> The output file for chromosome") 
+args= parser.parse_args()
+
+inputFile = open(args.infile, 'r')
+outputFile = open(args.outfile, 'w')
+
+for line in inputFile:
+    if line.startswith(">"):
+        line2 = line.split(",")
+        line2 = line2[0].split(" ")
+        
+        if(line2[4]=="22"):
+            print line
+            while True:
+                line=inputFile.next()
+
+                if (line.startswith(">")):
+                    break
+                    
+                outputFile.write(line)
+            
+            
+            
+            
+
