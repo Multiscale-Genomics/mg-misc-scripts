c89e5fbdb79cfd0eb3389aece10e28f5e659687a
rehamFatima <reham@ebi.ac.uk>
2018-10-31 15:31:10 +0000

    adding scripts for ATAC seq test data

---
+++ ATACSeq_Scripts/extract_chromosomeForATAC.py
@@ -0,0 +1,31 @@
+#!usr/bin/python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("infile", help="-infile -> The input fasta file")
+parser.add_argument("outfile", help="-outfile -> The output file for chromosome") 
+args = parser.parse_args()
+
+inputFile = open(args.infile, 'r')
+outputFile = open(args.outfile, 'w')
+
+for line in inputFile:
+    if line.startswith(">"):        
+        if("17" in line):
+            print line
+            outputFile.write(line)
+            while True:
+                line=inputFile.next()
+
+                if (line.startswith(">")):
+                    break
+                    
+                outputFile.write(line)
+            
+            
+            
+            
+inputFile.close()
+outputFile.close()
